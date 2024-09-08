7f24dc7c379d2bf238e244305f41577a16713f29
v76s <volkansisalan@gmail.com>
2024-09-08 23:48:37 +0300

    Bitwise operation update

--- Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
+++ Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
@@ -3,6 +3,8 @@
 int main()
 {
 	
+	// Access memory and update content
+	
 	uint8_t* ptr; 
 	ptr = (uint8_t*)0x20000000;
 	*ptr = 0x25 ; 
@@ -12,6 +14,11 @@ int main()
 	
 	ptr = ptr + 5 ; 
 	*ptr = tmp ; 
+	
+	// Update the memory address in bitwise form, we're done with ptr, overwrite it. 
+	
+	ptr = (uint8_t*)0x20001000; 
+	*ptr = (0x08 | 0x20) ;  // which is actually the OR operation for (00001000 | 00100000) = (00101000)			
 	
 	while(1)
 	{

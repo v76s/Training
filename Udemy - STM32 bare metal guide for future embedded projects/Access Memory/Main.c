28aa3ed25866546a91a99a3e675c5681e4ade8de
v76s <volkansisalan@gmail.com>
2024-09-08 23:59:10 +0300

    Updates to memory

--- Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
+++ Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
@@ -1,8 +1,7 @@
 #include <stdint.h>
 
 int main()
-{
-	
+{	
 	// Access memory and update content
 	
 	uint8_t* ptr; 
@@ -19,6 +18,13 @@ int main()
 	
 	ptr = (uint8_t*)0x20001000; 
 	*ptr |= (0x08 | 0x20) ;  // which is actually the OR operation for (00001000 | 00100000) = (00101000)			
+	
+	// Investigation of the endiness of the hardware	
+	uint16_t* ptr2 ; 
+	ptr2 = (uint16_t*)0x2000000;
+	*ptr2 = 0x219;  // where we have MSB = 0x02 and LSB = 0x19
+	ptr2++ ; 
+	*ptr2 = 0x219;  // We see that the 0x19 is loaded in memory earlier than 0x02
 	
 	while(1)
 	{

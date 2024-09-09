3ee5b1d691cc1f63c94bb0895297eaeba3927c3c
v76s <volkansisalan@gmail.com>
2024-09-09 08:48:31 +0300

    structure byte allocation in memory

--- Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
+++ Udemy - STM32 bare metal guide for future embedded projects/Access Memory/Main.c
@@ -21,10 +21,35 @@ int main()
 	
 	// Investigation of the endiness of the hardware	
 	uint16_t* ptr2 ; 
-	ptr2 = (uint16_t*)0x2000000;
+	ptr2 = (uint16_t*)0x20000000;
 	*ptr2 = 0x219;  // where we have MSB = 0x02 and LSB = 0x19
 	ptr2++ ; 
 	*ptr2 = 0x219;  // We see that the 0x19 is loaded in memory earlier than 0x02
+	
+	struct object
+	{
+		uint32_t bytes_4 ; 
+		uint16_t bytes_2 ; 
+		uint8_t  bytes_1a ; 
+		uint8_t  bytes_1b; 
+	};
+	
+	struct object* objPtr ; 
+	objPtr = (struct object*)0x20000000; 
+	
+	// 8 7 6 5 4 3 2 1
+	
+	// 8 7 6 5, 0x05060708; 
+	objPtr->bytes_4 = 0x05060708; 
+	
+	// 0x0304
+	objPtr->bytes_2 = 0x0304; 
+	
+	// 0x02
+	objPtr->bytes_1a = 0x02; 
+	
+	// 0x01
+	objPtr->bytes_1b = 0x01; 	
 	
 	while(1)
 	{
